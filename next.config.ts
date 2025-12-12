d1606e146bfd1dcec2b12cd8a01d82494e2cc07c
Ashwin-Pulipati <ashwinpulipati@gmail.com>
2025-12-12 02:06:23 -0500

    fis: Update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint:{
-    ignoreDuringBuilds: true,
-  }
+type NextConfigWithEslint = NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+};
+
+const nextConfig: NextConfigWithEslint = {
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
